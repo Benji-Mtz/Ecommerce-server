5721a82a22e4a1bb369ef98da9906b063ee73816
Benji-Mtz <49083076+Benji-Mtz@users.noreply.github.com>
2021-10-01 14:15:08 -0500

    primer commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '48b9b1fc760060d5c4722e69871ba467'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fc427b80-d6dc-4b49-a15a-cddb9c08199c'
+};
